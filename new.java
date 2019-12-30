f5a77ed2f857cc08d6edc910d3279dc3b7cd86d8
Saurabh Verma <saurabhvermaboss110@gmail.com>
2019-12-30 16:48:22 +0530

    post with body and params

---
+++ new.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+
+
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UserResource
+{
+	@Autowired
+	UsersRepository repo;
+	@GetMapping("users")
+	
+public List<Users> getuser()
+{
+	List<Users> users= (List<Users>) repo.findAll();
+	
+	return users;
+
+}
+	@PostMapping("users")
+	public Users postuser(@RequestBody Users users) {
+		 repo.save(users);
+		return users;
+	}
+		public Users postvalueuser(Users users)
+		{ 
+			repo.save(users);
+		return users;
+	}
+	@DeleteMapping("/users/{id}")
+	public String Deleteuser(@PathVariable int id)
+	{
+		Users u=repo.getOne(id);
+		repo.delete(u);
+		return "Deleted";
+	}
+	
+	@PutMapping("users")
+	public Users updatetuser(@RequestBody Users users) {
+		 repo.save(users);
+		return users;	
+	}
+} 
